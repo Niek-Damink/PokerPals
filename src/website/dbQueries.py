2ba9e86167a91ff9566c8d0523306c7acbd3e48f
s2990164 <n.p.damink@student.utwente.nl>
2024-04-26 12:31:42 +0200

    When event is today or tomorrow it says so

--- src/website/dbQueries.py
+++ src/website/dbQueries.py
@@ -223,10 +223,18 @@ def getEvents():
     year = datetime.datetime.today().strftime('%Y')
     month = datetime.datetime.today().strftime('%m')
     day = datetime.datetime.today().strftime('%d')
+    today = datetime.datetime.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    today = today.strftime('%d/%m/%Y')
+    tomorrow = tomorrow.strftime('%d/%m/%Y')
     all_events = Events.query.filter((cast(func.substr(Events.date, 7, 10),Integer) > year) |
                                       ((cast(func.substr(Events.date, 4, 5), Integer) > month) & (cast(func.substr(Events.date, 7, 10) == year, Integer))) | 
                                       ((cast(func.substr(Events.date, 1, 2), Integer) >= day) & (cast(func.substr(Events.date, 4, 5), Integer) == month) & (cast(func.substr(Events.date, 7, 10), Integer) == year))).order_by(cast(func.substr(Events.date, 7, 10),Integer).asc(), cast(func.substr(Events.date, 4, 5),Integer).asc(), cast(func.substr(Events.date, 1, 2),Integer).asc()).all()
-
+    for event in all_events:
+        if event.date == today:
+            event.date = "Today"
+        elif event.date == tomorrow:
+            event.date = "Tomorrow"
     return all_events
 
 def getSessionsForPerson(name):
